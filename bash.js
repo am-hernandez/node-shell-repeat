294e492b83b1b1c22714f5305f78e5a025c8ec40
am-hernandez <angel@mouseandweb.com>
2021-10-25 15:20:52 -0400

    implemented cat command

--- bash.js
+++ bash.js
@@ -1,16 +1,18 @@
 const ls = require("./ls");
 const pwd = require("./pwd");
+const cat = require("./cat");
 
 process.stdout.write("prompt >");
-// pwd()
 
 process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
-
-  switch (cmd) {
+  const [command, fileName] = cmd.split(" ");
+  switch (command) {
     case "pwd":
       return pwd();
     case "ls":
       return ls();
+    case "cat":
+      return cat(fileName);
   }
 });
